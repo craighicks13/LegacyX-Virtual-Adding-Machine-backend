e9d5487ea8077a6fb7566e7e2153cf8b8ba63537
craighicks13 <craighicks13@gmail.com>
2022-10-10 15:10:53 -0600

    added env port to server

--- src/server.js
+++ src/server.js
@@ -146,7 +146,7 @@ app.get('/api/journal/:id/', async (req, res) => {
 	res.send(journal);
 });
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
 	console.log('Server is listening on port ' + PORT);

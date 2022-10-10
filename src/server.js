0269fb857b5b225a5de24ca83d412834187a239b
craighicks13 <craighicks13@gmail.com>
2022-10-10 14:56:19 -0600

    update for build

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 let journals = [
 	{
@@ -73,8 +75,16 @@ let journals = [
 	},
 ];
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../build')));
+
+app.get(/^(?!\/api).+/, (req, res) => {
+	res.sendFile(path.join(__dirname, '../build/index.html'));
+});
 
 //TODO: Log in user
 export async function logInUser(user, password) {
